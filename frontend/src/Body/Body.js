94481048e93cf3971f3ad787540f20b081531623
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-03-10 21:09:27 +0900

    .

--- frontend/src/Body/Body.js
+++ frontend/src/Body/Body.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { AutoSizer } from 'react-virtualized';
 import { VariableSizeGrid as Grid } from 'react-window';
 import styles from './Body.module.css';
 import Content from './Content/Content';
@@ -8,22 +7,23 @@ function Body({ mainData }) {
   const cellWidth = 369;
   const cellHeight = 337;
   const columnCount = 6;
-  //const columnGap = 9;
+  const columnGap = 9;
   const rowGap = 10;
 
   const getCellSize = (index) => {
     const columnWidth = 352;
     const rowHeight = cellHeight + rowGap;
-    //const columnIndex = index % columnCount;
+    const columnIndex = index % columnCount;
     const rowIndex = Math.floor(index / columnCount);
 
-    //const left = columnIndex * (columnWidth + columnGap) + 20;
+    const left = columnIndex * (columnWidth + columnGap) + 20;
     const top = rowIndex * rowHeight + 20;
     const isLastRow = rowIndex === Math.floor((mainData.length - 1) / columnCount);
     const bottom = isLastRow ? 0 : rowHeight;
     return {
       width: columnWidth,
       height: cellHeight,
+      left,
       top,
       bottom,
     };
@@ -31,10 +31,10 @@ function Body({ mainData }) {
 
   const Cell = ({ columnIndex, rowIndex, style }) => {
     const index = rowIndex * columnCount + columnIndex;
-    const { width, height, top, bottom } = getCellSize(index);
+    const { width, height, top, left, bottom } = getCellSize(index);
 
     return (
-      <div style={{ ...style, top, bottom, width, height }}>
+      <div style={{ ...style, top, left, bottom, width, height }}>
         <Content
           id={mainData[index].id}
           title={mainData[index].title}
@@ -47,28 +47,18 @@ function Body({ mainData }) {
   };
 
   return (
-    <div
-      className={styles.BodyContainer}
-      style={{ display: 'block', width: '100%', height: '100vh', textAlign: 'center' }}
-    >
-      <AutoSizer>
-        {({ height, width }) => (
-          <div style={{ width, height }}>
-            <Grid
-              columnCount={columnCount}
-              columnWidth={() => cellWidth}
-              rowCount={Math.ceil(mainData.length / columnCount)}
-              rowHeight={() => cellHeight + rowGap}
-              height={height}
-              width={width}
-              className={styles.ContentContainer}
-              style={{ margin: '0 auto' }}
-            >
-              {Cell}
-            </Grid>
-          </div>
-        )}
-      </AutoSizer>
+    <div className={styles.BodyContainer}>
+      <Grid
+        columnCount={columnCount}
+        columnWidth={() => cellWidth}
+        rowCount={Math.ceil(mainData.length / columnCount)}
+        rowHeight={() => cellHeight + rowGap}
+        height={window.innerHeight - 40}
+        width={window.innerWidth}
+        className={styles.ContentContainer}
+      >
+        {Cell}
+      </Grid>
     </div>
   );
 }
